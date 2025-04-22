80212fa3e5d8dd1d0b060a556ea7a7591214f53f
KathirICBT <sanjayicbt@gmail.com>
2025-04-22 19:51:28 +0530

    route structure

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ app.get("/", (req, res) => {
   res.json({ msg: "Hello World!" });
 });
 
+// CRUD FUNCTIONALITY
+
+// FOR READING
+app.get("/students", (req, res) => {});
+// FOR CREATE
+app.post("/students", (req, res) => {});
+// FOR UPDATE
+app.put("/students/:id", (req, res) => {});
+// FOR DELETE
+app.delete("/students/:id", (req, res) => {});
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
