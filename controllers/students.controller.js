7faa569ec310c14f0bf2e42599dabc4ea3bb396c
KathirICBT <sanjayicbt@gmail.com>
2025-04-22 20:46:55 +0530

    add controller files

---
+++ controllers/students.controller.js
@@ -0,0 +1,15 @@
+export const StudentIndex = (req, res) => {
+  res.send("Get All Students");
+};
+
+export const StudentCreate = (req, res) => {
+  res.send("Add a new student");
+};
+
+export const StudentUpdate = (req, res) => {
+  res.send("Update a student");
+};
+
+export const StudentDelete = (req, res) => {
+  res.send("Delete a Student");
+};
--- routes/students.route.js
+++ routes/students.route.js
@@ -1,22 +1,23 @@
 import express from "express";
+import {
+  StudentCreate,
+  StudentDelete,
+  StudentIndex,
+  StudentUpdate,
+} from "../controllers/students.controller.js";
 
 const router = express.Router();
 
-// FOR READING
-router.get("/", (req, res) => {
-  res.send("Get All Students");
-});
-// FOR CREATE
-router.post("/", (req, res) => {
-  res.send("Add a new student");
-});
-// FOR UPDATE
-router.put("/:id", (req, res) => {
-  res.send("Update a student");
-});
-// FOR DELETE
-router.delete("/:id", (req, res) => {
-  res.send("Delete a Student");
-});
+// READ
+router.get("/", StudentIndex);
+
+// CREATE
+router.post("/", StudentCreate);
+
+// UPDATE
+router.put("/:id", StudentUpdate);
+
+// DELETE
+router.delete("/:id", StudentDelete);
 
 export default router;
